0f75cc0e17fe1799061e76aefbf3d0954f649f0a
zearakun <91083604+zearakun@users.noreply.github.com>
2022-10-09 01:16:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -23,8 +23,7 @@ def proxiesgen():
 
 
 
-for token in tokens:
-  threading.Thread(target=Join, args=(token)).start()
+
 
 
 headers2 = {
@@ -66,7 +65,7 @@ def dead(token):
   }
   if config["proxy"] == True:
     response = requests.post(f"https://discord.com/api/v6/invites/{random.choice(range(0,1000000))}",
-                             headers=headers,,
+                             headers=headers,
                              proxies=proxiesgen())
     if response.status_code == 200:
       return
@@ -91,26 +90,43 @@ def dead(token):
 def Join(token):
   properties = '{"os":"Windows","browser":"Chrome","device":"","system_locale":"ja","browser_user_agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36","browser_version":"103.0.0.0","os_version":"10","referrer":"","referring_domain":"","search_engine":"","referrer_current":"","referring_domain_current":"","release_channel":"stable","client_build_number":139674,"client_event_source":null}'
   headers = {
-    "authorization": token
+    "authorization": token,
+    "referer": "discord.com/channels/@me",
+    "sec-fetch-dest": "empty",
+    "x-debug-options": "bugReporterEnabled",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    "accept": "*/*",
+    "accept-language": "ja-JP",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/0.0.307 Chrome/78.0.3904.130 Electron/7.3.2 Safari/537.36",
+    "TE": "trailers",
+    "X-Super-Properties": base64.b64encode(properties.encode()),
   }
+  headers["x-fingerprint"] = getfingerprint()
   if config["proxy"] == True:
-    response = requests.post(f"https://discord.com/api/v6/invites/ctkp",
+    response = requests.post(f"https://discord.com/api/v10/invites/ctkp",
                              headers=headers,
                              cookies=getcookie(),
                              proxies=proxiesgen())
     if response.status_code == 200:
         while 1:threading.Thread(target=dead,args=(token,)).start()
     else:
-      if "captcha_rqdata" and "captcha_key" in response.text:
+      if "captcha_key" in response.text:
         while 1:threading.Thread(target=dead,args=(token,)).start()
+      else:
+        print("[-] DeadToken : " + token)
   else:
-    response = requests.post(f"https://discord.com/api/v6/invites/ctkp",
+    response = requests.post(f"https://discord.com/api/v10/invites/ctkp",
                              headers=headers,
                              cookies=getcookie())
     if response.status_code == 200:
         while 1:threading.Thread(target=dead,args=(token,)).start()
     else:
-      if "captcha_rqdata" and "captcha_key" in response.text:
+      if "captcha_key" in response.text:
         while 1:threading.Thread(target=dead,args=(token,)).start()
-  Setup()
-  Start()
+      else:
+        print("[-] DeadToken : " + token)
+
+for token in tokens:
+  threading.Thread(target=Join, args=(token,)).start()
+  print(token)
